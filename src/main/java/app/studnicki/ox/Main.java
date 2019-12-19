ff7d9263ff5237c4c624b33fa6e53e540a003429
alek.studnicki@gmail.com <alek.studnicki@gmail.com>
2019-12-19 11:24:00 +0100

    cleared main

--- src/main/java/app/studnicki/ox/Main.java
+++ src/main/java/app/studnicki/ox/Main.java
@@ -8,21 +8,5 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        UserInterface ui = UserInterfaceFactory.Console();
-
-        ui.welcome();
-        Config.getInstance().changeLanguage("pl");
-
-        new Scanner(System.in).nextLine();
-
-
-        ui.welcome();
-        Config.getInstance().changeLanguage("ja");
-
-        new Scanner(System.in).nextLine();
-
-        ui.welcome();
-
-        new Scanner(System.in).nextLine();
     }
 }
