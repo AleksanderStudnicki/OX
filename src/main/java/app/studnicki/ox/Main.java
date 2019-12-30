18a67fc570a81fdfced486eb2d638388cdef2496
alek.studnicki@gmail.com <alek.studnicki@gmail.com>
2019-12-30 15:07:55 +0100

    added size readed from args

--- src/main/java/app/studnicki/ox/Main.java
+++ src/main/java/app/studnicki/ox/Main.java
@@ -3,7 +3,21 @@ package app.studnicki.ox;
 class Main {
   public static void main(String[] args) {
     ConsoleUserInterface ui = new ConsoleUserInterface(System.in);
-    Round round = new Round(3);
+
+    int n;
+
+    if (args.length > 0) {
+      try {
+        n = Integer.parseInt(args[0]);
+      } catch (NumberFormatException ex) {
+        ui.error(ex.getMessage());
+        n = 3;
+      }
+    } else {
+      n = 3;
+    }
+
+    Round round = new Round(n);
 
     round.addObserver(ui);
 
