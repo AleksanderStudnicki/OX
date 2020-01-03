e1e2b053048dcd2cff60bbd437e9ad1285c3b362
alek.studnicki@gmail.com <alek.studnicki@gmail.com>
2020-01-03 12:39:40 +0100

    fix in python script

--- pipeTest.py
+++ pipeTest.py
@@ -1,13 +1,9 @@
 from subprocess import run, PIPE
 
 values = "0\n0\n0\n0\n1\n2\n3\n4\n5\n6\n\n0\n1\n2\n3\n4\n5\n6\n\n0\n1\n2\n3\n4\n5\n6\n\n"
-
-p = run(['java', '-jar', '/home/alekstudnicki/Kod/ox/target/ox-0.2.1.jar'],
+p = run(['java', '-jar', 'target/ox-0.2.1.jar'],
                    stdout=PIPE, encoding='utf-8', input=values, stderr=PIPE)
-output = p.stdout
-
-print(p.stderr)
-print(output)
-
-
-
+out = p.stdout
+err = p.stderr
+print(err)
+print(out)
