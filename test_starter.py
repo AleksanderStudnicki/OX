43ee07082c3da6458a64981c49a89d250c801e07
alek.studnicki@gmail.com <alek.studnicki@gmail.com>
2020-01-08 08:58:08 +0100

    validation in test starter

--- test_starter.py
+++ test_starter.py
@@ -4,14 +4,20 @@ from automatedTests.automated_tests import AutomatedTests
 
 
 def __parse_settings(args):
-    return Settings(args[1], args[2])
+    try:
+        dimension = int(args[1])
+        winning_rule = int(args[2])
+        return Settings(dimension, winning_rule)
+    except ValueError:
+        print("Not valid values!")
 
 
 def __start():
     if len(sys.argv) > 2:
         settings = __parse_settings(sys.argv)
-        automated_tests = AutomatedTests(settings)
-        automated_tests.run()
+        if settings is not None:
+            automated_tests = AutomatedTests(settings)
+            automated_tests.run()
 
 
 __start()
